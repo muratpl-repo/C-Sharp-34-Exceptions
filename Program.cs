7ce347e391a20b3cca4d7c1bee1962d60757fe3f
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:42:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _79DersExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 2, 3, 4 };
+            try
+            {
+                Console.WriteLine(sayilar[10]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Lutfen array sayisindandan buyuk bir deger girmeyiniz!");
+            }
+        }
+    }
+}
